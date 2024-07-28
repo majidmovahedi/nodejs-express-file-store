f62f1af0f4614ee644e8f5140a76016b151f0b8d
majidmovahedi <majidmovahedinasab@gmail.com>
2024-07-28 15:18:55 +0330

    Blog Category Validation

--- src/controllers/api/v1/blog/adminBlogController.ts
+++ src/controllers/api/v1/blog/adminBlogController.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-
 export class AdminBlogController {
 
     static async getBlog (req : Request , res : Response ) {
@@ -17,18 +16,22 @@ export class AdminBlogController {
     }
 
     static async createCategory (req : Request , res : Response ) {
+
         const { title } = req.body
         const result = await prisma.blogCategory.create({
             data: { title },
         }).then((result)=>{
-            res.status(201).json(result);
-        }).catch((error)=>{
-            if (error.code == "P2002"){
-                return res.status(409).json("This Category is Already Exist!")
-            }else{
-                return res.status(520).json("Unknown Error, Please Try Again Later.")
-            }
+            return res.status(201).json(result);
         })
+        // .catch((error)=>{
+        //     if (error.code == "P2002"){
+        //         return res.status(409).json("This Category is Already Exist!")
+        //     }else{
+        //         return res.status(520).json("Unknown Error, Please Try Again Later.")
+        //     }
+        // })
+
+
     }
 
     static async updateCategory (req : Request , res : Response ) {
--- src/routes/api/v1/blog/adminBlogRouter.ts
+++ src/routes/api/v1/blog/adminBlogRouter.ts
@@ -1,14 +1,16 @@
-import { Router , Request , Response } from "express";
-import { AdminBlogController } from "../../../../controllers/api/v1/blog/adminBlogController";
+import { Router } from "express";
+import { validateBody } from 'express-joi-validations';
 
+import { AdminBlogController } from "../../../../controllers/api/v1/blog/adminBlogController";
+import { schema } from "../../../../utils/validationBlogCategory";
 const router = Router();
 
 router.get('/', AdminBlogController.getBlog);
 
-
-router.get('/category', AdminBlogController.getCategory);
+router.get('/category', validateBody(schema) , AdminBlogController.getCategory);
 router.post('/category', AdminBlogController.createCategory);
 router.put('/category/:id', AdminBlogController.updateCategory);
 router.delete('/category/:id', AdminBlogController.deleteCategory);
+
 
 export default router;
