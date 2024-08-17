d7e7149a06432fa2bdd7e1f8761c8899b80a0b15
majidmovahedi <majidmovahedinasab@gmail.com>
2024-08-17 09:35:22 +0330

    Api swagger Structure with swagger.json

--- server.ts
+++ server.ts
@@ -3,9 +3,17 @@ import express from "express";
 import dotenv from "dotenv";
 import apiVersionRouter from "@routes/api/apiVersionRouter";
 import swaggerUi from "swagger-ui-express";
-import { specs } from "./swagger";
+import fs from "fs";
+import path from "path";
 
 const app = express();
+
+// Serve the Swagger API documentation
+const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, 'swagger.json'), 'utf8'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json({ type: 'application/json' }));
 
@@ -13,7 +21,7 @@ dotenv.config();
 const port = process.env.PORT;
 
 app.use('/api', apiVersionRouter);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
+
 
 app.listen( port , ()=>{
     console.log(`Listening On Port ${port}`)
--- src/routes/api/v1/blog/userBlogRouter.ts
+++ src/routes/api/v1/blog/userBlogRouter.ts
@@ -4,29 +4,6 @@ import { UserBlogController } from "@controllers/api/v1/blog/userBlogController"
 const router = Router();
 
 // User Blog Router
-
-/**
- * @swagger
- * /api/v1/blog:
- *   get:
- *     summary: Get a list of all blogs
- *     tags: [Blogs]
- *     responses:
- *       200:
- *         description: Successful response
- *         content:
- *           application/json:
- *             example:
- *               data: [{}]
- *       400:
- *         description: Bad Request
- *         content:
- *          application/json:
- *            example:
- *             error:
- *              message: "Bad Request"
- */
-//Get All Blogs
 router.get('/', UserBlogController.allBlog);
 router.get('/:id', UserBlogController.singleBlog);
 
