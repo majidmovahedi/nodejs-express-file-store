f121df84b79c753819e6930886a3a258534fe4fe
majidmovahedi <majidmovahedinasab@gmail.com>
2024-08-21 19:11:30 +0330

    change adminUserBlogController - updateCategory syntax to try-catch

--- src/controllers/api/v1/blog/adminBlogController.ts
+++ src/controllers/api/v1/blog/adminBlogController.ts
@@ -174,23 +174,33 @@ export class AdminBlogController {
         const { id } = req.params;
         const { title } = req.body;
 
-        const category = await prisma.blogCategory
-            .update({
+        try {
+            const category = await prisma.blogCategory.update({
                 where: { id: parseInt(id) },
                 data: { title },
-            })
-            .then((category) => {
-                return res.status(201).json(category);
-            })
-            .catch((error) => {
-                if (error.code == 'P2025') {
-                    return res.status(409).json('This Id is Not Exist!');
-                } else {
-                    return res
-                        .status(520)
-                        .json('Unknown Error, Please Try Again Later.');
-                }
             });
+
+            return res.status(200).json(category);
+        } catch (error) {
+            const prismaError = error as CustomError;
+            if (prismaError.code === 'P2025') {
+                return res.status(404).json({
+                    message: 'This Id does not exist!',
+                    code: prismaError.code,
+                });
+            }else if (prismaError.code === 'P2002') {
+                return res.status(404).json({
+                    message: 'This Category is Already Exists!',
+                    code: prismaError.code,
+                });
+            } else {
+                console.error('Unexpected error:', prismaError);
+                return res.status(520).json({
+                    message: 'Unknown error, please try again later.',
+                    details: prismaError.message,
+                });
+            }
+        }
     }
 
     async deleteCategory(req: Request, res: Response) {
