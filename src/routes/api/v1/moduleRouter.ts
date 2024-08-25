3317c584c2d1424199728888c7a5a20b21688ba4
majidmovahedi <majidmovahedinasab@gmail.com>
2024-08-25 13:30:56 +0330

    add shop route to moduleRouter

--- src/routes/api/v1/moduleRouter.ts
+++ src/routes/api/v1/moduleRouter.ts
@@ -3,11 +3,16 @@ import userBlogRouter from './blog/userBlogRouter';
 import adminBlogRouter from './blog/adminBlogRouter';
 import userRouter from './user/userRouter';
 import adminUserRouter from './user/adminUserRouter';
+// import shopRouter from './shop/shopRouter';
+import adminShopRouter from './shop/adminShopRouter';
 
 const router = Router();
 
 router.use('/blog', userBlogRouter);
 router.use('/admin/blog', adminBlogRouter);
+
+// router.use('/shop', userBlogRouter);
+router.use('/admin/shop', adminShopRouter);
 
 router.use('/user', userRouter);
 router.use('/admin/user', adminUserRouter);
