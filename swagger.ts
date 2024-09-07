121392a0426095e5fb18105d2239741e21595116
majidmovahedi <majidmovahedinasab@gmail.com>
2024-09-07 11:26:31 +0330

    create swagger-autogen - without Jwt

---
+++ swagger.ts
@@ -0,0 +1,33 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    version: '',            // by default: '1.0.0'
+    title: '',              // by default: 'REST API'
+    description: ''         // by default: ''
+  },
+  servers: [
+    {
+      url: 'http://localhost:8000',              // by default: 'http://localhost:3000'
+      description: ''       // by default: ''
+    },
+    // { ... }
+  ],
+  tags: [                   // by default: empty Array
+    {
+      name: '',             // Tag name
+      description: ''       // Tag description
+    },
+    // { ... }
+  ],
+  components: {}            // by default: empty object
+};
+
+const outputFile = './swagger.json';
+const routes = ['./src/routes/api/*.ts'];
+
+swaggerAutogen({openapi: '3.0.0'})(outputFile, routes, doc);
+
+// swaggerAutogen()(outputFile, routes, doc).then(async () => {
+//     await import('./server'); // Your project's root file
+//   });
