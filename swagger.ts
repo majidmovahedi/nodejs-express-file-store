3c92b65e7e6fc84aa2c0af6ffa1ff912482e4531
majidmovahedi <majidmovahedinasab@gmail.com>
2024-09-07 11:42:59 +0330

    swagger auth one endpoint

--- swagger.ts
+++ swagger.ts
@@ -1,32 +1,41 @@
-import swaggerAutogen from 'swagger-autogen';
+import swaggerAutogen from "swagger-autogen";
 
 const doc = {
   info: {
-    version: '',            // by default: '1.0.0'
-    title: '',              // by default: 'REST API'
-    description: ''         // by default: ''
+    version: "", // by default: '1.0.0'
+    title: "", // by default: 'REST API'
+    description: "", // by default: ''
   },
   servers: [
     {
-      url: 'http://localhost:8000',              // by default: 'http://localhost:3000'
-      description: ''       // by default: ''
+      url: "http://localhost:8000", // by default: 'http://localhost:3000'
+      description: "", // by default: ''
     },
     // { ... }
   ],
-  tags: [                   // by default: empty Array
+  tags: [
+    // by default: empty Array
     {
-      name: '',             // Tag name
-      description: ''       // Tag description
+      name: "", // Tag name
+      description: "", // Tag description
     },
     // { ... }
   ],
-  components: {}            // by default: empty object
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+      },
+    },
+  },
 };
 
-const outputFile = './swagger.json';
-const routes = ['./src/routes/api/*.ts'];
+const outputFile = "./swagger.json";
+const routes = ["./src/routes/api/*.ts"];
 
-swaggerAutogen({openapi: '3.0.0'})(outputFile, routes, doc);
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, routes, doc);
 
 // swaggerAutogen()(outputFile, routes, doc).then(async () => {
 //     await import('./server'); // Your project's root file
