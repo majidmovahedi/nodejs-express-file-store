88fc3cadc8eb49baa8a0891bdcb5b7f464c64e0f
Majid Movahedi <majidmovahedinasab@gmail.com>
2024-11-02 16:22:41 +0330

    delete some file and directory

--- src/utils/validation/customValidation.ts
+++
@@ -1,9 +0,0 @@
-// import { ExpressValidator } from 'express-validator';
-
-// const { body, validationResult } = new ExpressValidator(
-//   {
-//     isImageUrl: async (value: any) => {
-
-//     },
-//   },
-// );
